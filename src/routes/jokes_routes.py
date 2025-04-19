fc98a67e13c2f488d139b2000c0ab042eb6fe3eb
Akewushola abdulbakri temitope <neaterry6@gmail.com>
2025-04-19 03:55:09 +0100

    Update jokes_routes.py

--- src/routes/jokes_routes.py
+++ src/routes/jokes_routes.py
@@ -1,1 +1,9 @@
+import pyjokes
+from flask import Blueprint, jsonify
 
+bp = Blueprint("jokes_routes", __name__)
+
+@bp.route("/", methods=["GET"])
+def get_joke():
+    joke = pyjokes.get_joke()
+    return jsonify({"joke": joke})
